21e8b81f1442176a80ed75038afb4c0f08cec3d3
Nestor Fernandez <extraertond@gmail.com>
2022-05-01 01:46:27 +0200

    updated email service

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,13 +1,13 @@
 import emailjs from "@emailjs/browser";
 
-export const sendEmail = async (body: object) => {
+export const sendEmail = async (body: Record<string, unknown> | undefined) => {
     const serviceId = process.env.REACT_APP_EMAIL_SERVICE_ID as string;
     const templateId = process.env.REACT_APP_EMAIL_TEMPLATE_ID as string;
     const userId = process.env.REACT_APP_EMAIL_USER_ID as string;
 
     try {
         // status 200 ok
-        const response = await emailjs.send(serviceId, templateId, {firstname: "nenene", lastname: "amogus"}, userId);
+        const response = await emailjs.send(serviceId, templateId, body, userId);
         return response.status;
     } catch (e) {
         console.log(e);
